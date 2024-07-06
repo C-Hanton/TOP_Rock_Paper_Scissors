79f63d584a7a0a6e5c708c7ab3ae2f1e1fc917d4
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-07-06 13:37:43 +0100

    add pseudocode to script.js to explain steps taken in project

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-alert("This alert box was called with the onload event");
+/*alert("This alert box was called with the onload event"); */
+
+/* Pseudocodoe:
+
+Rock Paper Scissors Game
+
+1. Write the logic to get the computers choice
+ - write a function that generates a result of rock paper or scissors
+ - test the function with console.log()
+
+2. Write the logic to get the players choice
+ - Write a function that takes a human choice and returns it
+ - use prompt to get the users input
+ - test the returned result with console.log()
+
+ 3. Declare player score variables
+ - declare two variables: getHumanScore and getComputerScore and initialize to 0
+
+ 4. Write the logic to play a single round
+ - Create a function called playRound
+ - give playRound two parameters (humanChocie, computerChoice)
+ - make human chocie parameter case sentitive so that players can add rock ROCK rOcK etc
+ - Write code for playRound function to console.log() representation of the winner of the round such as "You win, paper beats rock" etc
+ -Increment the human score or computer score based on the winner
+
+ 5. Write logic to play entire game
+ -Create new function called playGame
+ -move playRound and score variables into playGame
+ -play 5 rounds by calling playGame 5 times */ 
+
